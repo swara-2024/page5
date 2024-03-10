4c2da0e69338cd9f675a2e6b8e9dbf9965a28a76
swara-2024 <162478520+swara-2024@users.noreply.github.com>
2024-03-10 18:45:17 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "What do we use to write on paper?",
+      answer: "Pencil",
+      options: [
+      "Pencil",
+      "Pensil",
+      "Pensill",
+      "Pensul"
+      ]
+  },
+  {
+  numb: 2,
+  question: "Where do we sit and learn in school?",
+  answer: "Desk",
+  options: [
+  "Desk",
+  "Desck",
+  "Dask",
+  "Dexk"
+  ]
+},
+  
+   {
+      numb: 3,
+      question: "What do we use to erase mistakes on paper?",
+      answer: "Eraser",
+      options: [
+      "Eraser",
+      "Iraser",
+      "Araser",
+      "Erraser"
+      ]
+  },
+    {
+      numb: 4,
+      question: "Where do we keep our books in school?",
+      answer: "Backpack",
+      options: [
+      "Backpack",
+      "Backpak",
+      "Bakpack",
+      "Bakpak"
+      ]
+  },
+    {
+      numb: 5,
+      question: "What do we use to fasten papers together?",
+      answer: "Stapler",
+      options: [
+      "Stapler",
+      "Staplar",
+      "Stapuler",
+      "Staplur"
+    ]
+  },
+
+];
